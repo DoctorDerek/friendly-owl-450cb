49144de50566a8e6eda070928f326743ad0448b1
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-05 18:56:59 -0500

    First attempt at Auth0 login

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,45 @@
-const siteMetadata = require('./site-metadata.json')
+const siteMetadata = require("./site-metadata.json")
 
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: siteMetadata,
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        `gatsby-transformer-remark`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`
-            }
-        },
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {}
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {}
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-            }
-        }
-    ]
-};
+  pathPrefix: "/",
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-source-data`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {},
+    },
+    {
+      resolve: `@stackbit/gatsby-plugin-menus`,
+      options: {
+        sourceUrlPath: `fields.url`,
+        pageContextProperty: `menus`,
+      },
+    },
+    {
+      resolve: "gatsby-theme-auth0",
+      options: {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        redirectUri: process.env.AUTH0_CALLBACK_URL,
+        // audience: process.env.AUTH0_AUDIENCE, // Optional
+        // responseType: process.env.AUTH0_RESPONSE_TYPE, // Optional
+        // scope: process.env.AUTH0_SCOPE, // Optional
+        // callbackPath: "/auth/callback", // Optional
+      },
+    },
+  ],
+}
