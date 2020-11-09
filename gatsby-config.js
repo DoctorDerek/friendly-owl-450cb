2687b748445f96c6488b0aa629279c64ee56c08b
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-09 13:26:25 -0500

    Change to comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,15 +34,16 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-theme-auth0",
+      resolve: `gatsby-theme-auth0`,
       options: {
         domain: process.env.AUTH0_DOMAIN,
         clientID: process.env.AUTH0_CLIENT_ID,
         redirectUri: process.env.AUTH0_CALLBACK_URL,
-        // audience: process.env.AUTH0_AUDIENCE, // Optional
-        // responseType: process.env.AUTH0_RESPONSE_TYPE, // Optional
-        // scope: process.env.AUTH0_SCOPE, // Optional
-        // callbackPath: "/auth/callback", // Optional
+        // Optional fields:
+        // audience: process.env.AUTH0_AUDIENCE,
+        // responseType: process.env.AUTH0_RESPONSE_TYPE,
+        // scope: process.env.AUTH0_SCOPE,
+        // callbackPath: "/auth/callback",
       },
     },
   ],
